324f100b78cea7679c47dbb2702e562bde9599ce
EEEios <pd90109097@163.com>
2020-05-08 18:53:45 +0800

    添加课设工程 1. 构建好基础的fxml界面 2. 进行treeview的实验（位于PictureOverviewController中

---
+++ viewerByPanD/src/viewer/Main.java
@@ -0,0 +1,57 @@
+package viewer;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        this.primaryStage = primaryStage;
+        primaryStage.setTitle("AddressApp");
+        this.primaryStage.getIcons().add(new Image("file:resources/images/title.png"));
+        initRootLayout();
+        showPictureOverview();
+    }
+
+    public void initRootLayout(){
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane)loader.load();
+
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showPictureOverview() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/PictureOverview.fxml"));
+            AnchorPane pictureOverview = (AnchorPane) loader.load();
+
+//            AnchorPane pictureOverview = FXMLLoader.load(Main.class.getResource("view/PictureOverview.fxml"));
+            rootLayout.setCenter(pictureOverview);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
